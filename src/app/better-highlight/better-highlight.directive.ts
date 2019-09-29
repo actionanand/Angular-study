c60b803f2f9f40d312b3e3aa5f88731daf3bfbee
actionanand <meetanandns@gmail.com>
2019-09-29 16:58:23 +0530

    HostBinder is used for better performance

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,10 +1,12 @@
-import { Directive, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
   
+  @HostBinding('style.backgroundColor') bgColor: string = 'transparent';
+
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(){
@@ -13,13 +15,15 @@ export class BetterHighlightDirective implements OnInit{
   }
 
   @HostListener('mouseenter') mouseOver(eventData: Event){
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color',
-    'lightgreen');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color',
+    // 'lightgreen');
+    this.bgColor = 'lightgreen';
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event){
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color',
-    'transparent');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color',
+    // 'transparent');
+    this.bgColor = 'transparent';
   }
 
 }
