90c1a3c23abab67b5158f601fcaf156713d09d60
actionanand <meetanandns@gmail.com>
2019-09-29 16:35:18 +0530

    HostListener Decorator is used to listen to mouse event

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,25 @@
-import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
-
+  
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(){
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color',
+    // 'yellow');
+  }
+
+  @HostListener('mouseenter') mouseOver(eventData: Event){
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color',
+    'lightgreen');
+  }
+
+  @HostListener('mouseleave') mouseLeave(eventData: Event){
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color',
+    'transparent');
   }
 
 }
