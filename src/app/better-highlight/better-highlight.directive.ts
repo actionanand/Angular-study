8edaf1e026bd365de7aa8c7b831b569764785d19
actionanand <meetanandns@gmail.com>
2019-09-29 17:23:21 +0530

    getting color output from outside using Input decorator

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,29 +1,32 @@
-import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
   
-  @HostBinding('style.backgroundColor') bgColor: string = 'transparent';
-
+  @Input() defaultColor: string = 'transparent'
+  @Input('appBetterHighlight') highlightColor: string;
+  @HostBinding('style.backgroundColor') bgColor: string;
+  
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(){
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color',
     // 'yellow');
+    this.bgColor=this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseOver(eventData: Event){
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color',
     // 'lightgreen');
-    this.bgColor = 'lightgreen';
+    this.bgColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event){
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color',
     // 'transparent');
-    this.bgColor = 'transparent';
+    this.bgColor = this.defaultColor;
   }
 
 }
